c266ec5704c35504712c44ceefdba504500691d0
Samarth Parnami <samarth.p@uniqode.com>
2026-01-12 06:51:11 +0530

    Change client key

--- insights.js
+++ insights.js
@@ -4,7 +4,7 @@
     script.src = 'https://q.cdn.uniqode.com/insights/staging/v1/latest/sdk.js';
     script.onload = function() {
       window.insights.init({
-        clientKey: 'cl_9aebdb05-94ce-48b8-9f44-098564b20f93'
+        clientKey: 'cl_729c1d2e-e58b-4b29-93e3-722e148e2f7f'
       });
     };
     document.head.appendChild(script);
