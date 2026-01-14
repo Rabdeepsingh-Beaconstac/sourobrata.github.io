15b60363faf905e5ce0fc8b021ca4e1443f07a73
Rabdeep Singh Kharbanda <100186839+Rabdeepsingh-Beaconstac@users.noreply.github.com>
2026-01-14 15:33:23 +0530

    Comment out Uniqode Insights SDK loading code
    Comment out the Uniqode Insights SDK initialization code.

--- insights.js
+++ insights.js
@@ -1,11 +1,11 @@
-// Load and initialize Uniqode Insights SDK
-(function() {
-    var script = document.createElement('script');
-    script.src = 'https://q.cdn.uniqode.com/insights/staging/v1/latest/sdk.js';
-    script.onload = function() {
-      window.insights.init({
-        clientKey: 'cl_729c1d2e-e58b-4b29-93e3-722e148e2f7f'
-      });
-    };
-    document.head.appendChild(script);
-  })();
+// // Load and initialize Uniqode Insights SDK
+// (function() {
+//     var script = document.createElement('script');
+//     script.src = 'https://q.cdn.uniqode.com/insights/staging/v1/latest/sdk.js';
+//     script.onload = function() {
+//       window.insights.init({
+//         clientKey: 'cl_729c1d2e-e58b-4b29-93e3-722e148e2f7f'
+//       });
+//     };
+//     document.head.appendChild(script);
+//   })();
